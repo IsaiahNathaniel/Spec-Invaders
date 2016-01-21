4c9ac97d84da2645997433315bfb507edb29b41d
IsaiahNathaniel <isaiah712.minniear@gmail.com>
2016-01-21 16:38:21 -0500

    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
 
 // main state of game
 var mainState = {
@@ -7,66 +7,103 @@ var mainState = {
     preload: function() {
         // background color
         game.stage.backgroundColor = '#71c5cf';
-        // load bird asset
-        game.load.image('bird', 'assets/bird.png');    
-        game.load.image('pipe', 'assets/pipe.png');
+        // load asset
+        game.load.image('player', 'assets/invader.png');    
+        // game.load.image('pipe', 'assets/.png');
     },
     
     // set up game, display sprites, etc
     create: function() {
         // set physics
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        // display bird
-        this.bird = this.game.add.sprite(100, 245, 'bird');
-        // add gravity to bird
-        game.physics.arcade.enable(this.bird)
-        this.bird.body.gravity.y = 1000;
-        // jump function
-        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this);
-        // pipes
-        this.pipes = game.add.group(); //create a group
-        this.pipes.enableBody = true; // add physics
-        this.pipes.createMultiple(20, 'pipe');
-        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); // game timer
+        
+        // display ship
+        this.player = this.game.add.sprite(400, 500, 'player');
+        game.physics.arcade.enable(this.player)
+        this.player.anchor.setTo(0.5, 0.5); // where the "middle" of the player is
+      
+        // creating the enemies in a group
+        this.baddies = game.add.group(); 
+        this.baddies.enableBody = true; // enables collision
+        this.baddies.physicsBodyType = Phaser.Physics.ARCADE;
+        this.baddies.setAll('anchor.x', 0.5); // needed for collision info
+        this.baddies.setAll('anchor.y', 0.5);
+        this.baddies.collideWorldBounds=true;
+        this.baddies.enableBody
+        
+        
+        // creating the projectiles being shot from our player
+        this.projectiles = game.add.group();
+        this.projectiles.enableBody = true; // enables collision
+        this.projectiles.physicsBodyType = Phaser.Physics.ARCADE;
+        this.projectiles.createMultiple(30, 'projectile') // create alot of projectiles (we will re-use the projectiles in order to save on space)
+        this.projectiles.setAll('outOfBoundsKill', true); // kill if bullets leave screen
+        this.projectiles.setAll('checkWorldBounds', true); // allow bullets to detect edges
+        this.projectiles.setAll('anchor.x', 0.5); // this is needed for collision information
+        this.projectiles.setAll('anchor.y', 1);
+        
+        
+        //this next function will be called from below and create the baddies
+        this.createBaddies();
+        
+        // setting the variable movement to a pre-packaged input set, bound to cursor keys.
+        this.movement = game.input.keyboard.createCursorKeys();
+        
+        // uses phasers built in event listener(basically checks for input) to bind to space
+        this.fireProjectile = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        
+        // score keeping
+        
         this.score = 0;
-        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });  
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+        
+        
+        
+        //this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+         
     },
     
     // called 60 times per second contains games logic
     update: function() {
-        // checks for world leaving
-        if (this.bird.inWorld == false)
-            this.restartGame();
-        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+    
+        // constantly sets velocity to 0, needed because we used velocity changes as our method of movement, this makes it so the player doesn't keep sliding in one direction
+        this.player.body.velocity.setTo(0,0);
+        
+        // if left cursor is down, go left, if right is down, go right. uses velocity from this frameworks built-in physics
+        if (this.movement.left.isDown){
+            this.player.body.velocity.x = -200;
+        }
+        else if (this.movement.right.isDown){
+            this.player.body.velocity.x = 200;
+        }
+        
+        // basically says if space key is pressed then run the shootProjectile function
+        if (this.fireProjectile.isDown){
+            shootProjectile();
+        }
+        
+        //
+       // if (this.aliens.x > 0 && this.aliens.x < 800)
+        
+        
     },
-    jump: function() {
-        //adds vertical velocity
-        this.bird.body.velocity.y = -350;
+    
+    createBaddies: function() {
+        // add an array of sorts of baddies
+        for (var y = 0; y < 4; y++) {
+            for (var x = 0; x < 8; x++){
+                this.baddies.create(x * 90,y * 50, 'invader');
+            }
+        }
+        
+        this.baddies.x = 70;
+        this.baddies.y = 50;
     },
+    
     restartGame: function() {
-        // Restarts gamae
+        // Restarts game
         game.state.start('main')
     },
-    addOnePipe: function(x, y) {
-        var pipe = this.pipes.getFirstDead(); //gets first dead
-        pipe.reset(x, y); //set new position of pipe
-        pipe.body.velocity.x = -200; // make it move to the left
-        // kill when no longer visible
-        pipe.checkWorldBounds = true;
-        pipe.outOfBoundsKill = true;
-    },
-    addRowOfPipes: function() {
-        // Pick floor hole.
-        var hole = Math.floor(Math.random() * 5) + 1;
-        // add 6 pipes
-        for (var i = 0; i < 8; i++)
-            if (i != hole && i != hole + 1 && i != hole + 2)
-                this.addOnePipe(400, i * 65 + 10);
-        this.score += 1;
-        this.labelScore.text = this.score;
-        
-    }
 };
 
 game.state.add('main', mainState);
