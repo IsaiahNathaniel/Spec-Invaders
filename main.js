e946cb33c4e13841bc0f42bd0f382b1263f32802
IsaiahNathaniel <isaiah712.minniear@gmail.com>
2016-01-21 23:27:11 -0500

    void

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         // display ship
-        this.player = this.game.add.sprite(400, 500, 'player');
+        this.player = this.game.add.sprite(400, 550, 'player');
         game.physics.arcade.enable(this.player)
         this.player.anchor.setTo(0.5, 0.5); // where the "middle" of the player is
       
@@ -28,8 +28,7 @@ var mainState = {
         this.baddies.physicsBodyType = Phaser.Physics.ARCADE;
         this.baddies.setAll('anchor.x', 0.5); // needed for collision info
         this.baddies.setAll('anchor.y', 0.5);
-        this.baddies.collideWorldBounds=true;
-        this.baddies.enableBody
+        this.baddies.collideWorldBounds=true; // so player can't leave the screen
         
         
         // creating the projectiles being shot from our player
@@ -59,7 +58,7 @@ var mainState = {
         
         
         
-        //this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        //this.timer = game.time.events.loop(1500, this.hjsdala, this);
          
     },
     
@@ -81,23 +80,64 @@ var mainState = {
         if (this.fireProjectile.isDown){
             shootProjectile();
         }
+                                                  
+        if (this.baddies.x > 0 && this.baddies.x < 20) {
+            
+            this.baddiesMoveRight();
+            
+        }
         
-        //
-       // if (this.aliens.x > 0 && this.aliens.x < 800)
+        if (this.baddies.x > 780 && this.baddies.x < 800) {
+            
+            this.baddiesMoveLeft();
+            
+        }
         
+        console.log(this.baddies.y)
         
     },
     
     createBaddies: function() {
         // add an array of sorts of baddies
         for (var y = 0; y < 4; y++) {
-            for (var x = 0; x < 8; x++){
+            for (var x = 0; x < 6; x++){
                 this.baddies.create(x * 90,y * 50, 'invader');
             }
         }
         
         this.baddies.x = 70;
         this.baddies.y = 50;
+        
+        // alright, so this is a little hard to explain, but this is a tween, basically it helps with fluid animation, and in this case, is moving our baddies back and forth, I'm actually going to break up each part on a seperate line and explain it
+        var screenMovement = game.add.tween(this.baddies).to(
+            { x: 200 },  // we are going to be moving the entire group of baddies to the x position of 200 from its current position
+            2000, // it should take 2000 milliseconds for it to reach each side
+            Phaser.Easing.Linear.None,  // this is the rate or style of change, we have it set to being casual
+            true, // this enables autostart
+            0,  // there is no delay, so this parameter is 0
+            1000, // repeat this 1000 times
+            true // this enables "yoyo'ing" basically, when our tween is done, this reverses it and plays it backwards
+        );
+        
+        screenMovement.onLoop.add(this.baddiesMoveDown, this);
+    },
+    
+    baddiesMoveDown: function() {
+        
+        this.baddies.y += 5;
+        
+    },
+        
+    baddiesMoveRight: function() {
+        
+        // reserved method for future use (maybe)
+        
+    },
+    
+    baddiesMoveLeft: function() {
+        
+        // reserved method for future use (maybe)
+        
     },
     
     restartGame: function() {
